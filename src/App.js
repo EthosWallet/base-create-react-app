1b30f7d7892b5875538f0d12d33686d31ec6f29b
NΞ◎N <90662438+i-am-neon@users.noreply.github.com>
2023-12-28 17:13:34 -0600

    add failing test and lint warning

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 
 function App() {
   const unused = 'unused variable';
+  const unused2 = 'unused variable';
   return (
     <div className="App">
       <header className="App-header">
